654b38d5fcbed0071faebf3c9f7665f9b6d6edf7
TEERAPONG_K <oppoalien@gmail.com>
2023-10-13 12:20:55 -0600

    fix: vite.config edit base '/xxxx' to './xxxx'

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/femt-interactive-rating-component",
+  base: "./femt-interactive-rating-component",
   plugins: [react()],
 });
